6f14722dec56ce4e52e27ad1fb0e1c9dbb80f544
skylli <lixiongsheng@163.com>
2016-08-30 15:06:04 +0800

    update code and readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# demo-c-doc4
-sample counter base wilddog-c-sdk
+# demo-c-doc 
+##远程计算器
+本部分展示了借助 Wilddog C/嵌入式 SDK 通过网页输入两个数，客户端对这两个数进行相加并推送到网页端。这是一个简单的远程加法器，它的输入和输出都是网页端，而控制逻辑也是简单的相加。你可以利用 Arduino 或树莓派 (可以在快速入门了解 Wilddog C/嵌入式 SDK 支持的平台 )，把各类的传感器的输出或者网上的天气等的数据作为其输入，电机驱动或者是 LCD 驱动作为其输出，制作更有实用意义的控制器。
+
+### 克隆代码
+
+    git clone https://github.com/skylli/demo-c-doc4.git
+
+### 使用
+1、把 demo-c-doc.c 拷贝到 wilddog-client-c\examples\linux\ 下，点击此下载[wilddog-client-c SDK](https://github.com/WildDogTeam/wilddog-client-c.git) 。
+2、编译：
+	$ make
+    $ make examples
+3、使用计算器
+    $./bin/demo-c-doc
+
+### 更多示例
+
+这里分类汇总了 WildDog 平台上的示例程序和开源应用，　链接地址：[https://github.com/WildDogTeam/wilddog-demos](https://github.com/WildDogTeam/wilddog-demos)
+
+### 相关文档
+
+* [Wilddog 概览](https://z.wilddog.com/overview/introduction)
+* [c SDK快速入门](https://z.wilddog.com/c/quickstart)
+* [c SDK API](https://z.wilddog.com/c/api)
+* [下载页面](https://www.wilddog.com/download/)
+* [Wilddog FAQ](https://z.wilddog.com/questions)
+
+### License
+MIT
+http://wilddog.mit-license.org/
---
+++ demo-c-doc.c
@@ -0,0 +1,143 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h> 
+#include "wilddog.h"
+
+#define DEMO_AUGEND "augend"
+#define DEMO_ADDEND "addend"
+#define DEMO_SUM "sum"
+/* modify to you url */
+#define DEMO_YOUR_URL "coap://c_test.wilddogio.com"
+
+/* sum set call back */
+STATIC void sumSet_callback(void *arg,Wilddog_Return_T err)
+{
+    if(err < WILDDOG_HTTP_OK || err >= WILDDOG_HTTP_NOT_MODIFIED)
+    {
+        wilddog_debug("set failed, err = %d!",err);
+        return;
+    }
+	
+	return;
+}
+/* get node pointer according the key value */
+STATIC Wilddog_Node_T *node_get(Wilddog_Node_T *p_head,const char* key_name)
+{
+	Wilddog_Node_T *node = p_head;
+	u8 len = 0;
+	while(node )
+	{
+		if( node->p_wn_key )
+		{
+			/* get the short len*/
+			len = strlen((const char*)key_name)>strlen((const char*)node->p_wn_key)\
+					?strlen((const char*)node->p_wn_key):strlen((const char*)key_name);
+			if( !memcmp(key_name,node->p_wn_key,len))
+				return node;
+		}
+		node=node->p_wn_next;
+		if(node == p_head)
+			return NULL;
+	}
+	return NULL;
+}
+/* count sum and set to server */
+STATIC Wilddog_Return_T sum_count(const Wilddog_Node_T *p_sn,Wilddog_T wilddog)
+{
+    Wilddog_Node_T *p_node,*p_augend,*p_addend,*p_sum;
+	int len ;
+	if( !p_sn || !p_sn->p_wn_child)
+		return WILDDOG_ERR_NULL;
+	wilddog_debug_printnode(p_sn);
+	/*find sum/augend/addend node */
+	p_node = p_sn->p_wn_child;
+	p_augend = node_get(p_node,"augend");
+	p_addend = node_get(p_node,"append");
+	p_sum = node_get(p_node,"sum");
+	if( !p_augend || !p_addend || !p_sum )
+	{
+		wilddog_debug("deficiency of data !");
+		return WILDDOG_ERR_NULL;
+	}
+	/* add */
+	s32 augend = *(s32*)wilddog_node_getValue(p_augend,&len);	
+	s32 addend = *(s32*)wilddog_node_getValue(p_addend,&len);
+	s32 sum = augend + addend;
+	wilddog_node_setValue(p_sum,(u8*)&sum,sizeof(sum));
+	printf("sum count : %ld + %ld = %ld\n",augend,addend,sum);
+	wilddog_setValue(wilddog,(Wilddog_Node_T*)p_sn,sumSet_callback,NULL);
+
+	return WILDDOG_ERR_NOERR;
+}
+/* notify call back  */
+STATIC void addObserver_callback
+    (
+    const Wilddog_Node_T* p_snapshot, 
+    void* arg,
+    Wilddog_Return_T err
+    )
+{
+	Wilddog_T wilddog = (Wilddog_T )arg;
+     if((err < WILDDOG_HTTP_OK || err >= WILDDOG_HTTP_NOT_MODIFIED ) &&
+        err != WILDDOG_ERR_RECONNECT)
+    {
+        wilddog_debug("addObserver failed! error code = %d",err);
+        return;
+    }
+    printf("Observe new data! \n");
+	if(p_snapshot && wilddog)
+    	sum_count(p_snapshot,wilddog);
+
+    return;
+}
+
+STATIC void set_callback(void *arg,Wilddog_Return_T err)
+{
+    if(err < WILDDOG_HTTP_OK || err >= WILDDOG_HTTP_NOT_MODIFIED)
+    {
+        wilddog_debug("tree creat fault !");
+        return;
+    }
+    printf("tree creat success! \n");
+    *(BOOL*)arg = TRUE;
+    return;
+}
+int main(void){
+	
+    BOOL set_finish = FALSE;
+    Wilddog_T wilddog = 0;
+    Wilddog_Node_T *p_head,*p_augend,*p_addend,*p_sum;
+
+    /*creat wilddog*/
+    wilddog = wilddog_initWithUrl((Wilddog_Str_T*)DEMO_YOUR_URL);
+	/* creat head node */ 
+    p_head = wilddog_node_createObject(NULL);
+	/* creat augend node */
+    p_augend = wilddog_node_createNum((Wilddog_Str_T*)DEMO_AUGEND,0);
+	/* creat addend node */    
+    p_addend = wilddog_node_createNum((Wilddog_Str_T*)DEMO_ADDEND,0);
+	/* creat sum node */
+    p_sum = wilddog_node_createNum((Wilddog_Str_T*)DEMO_SUM,0);
+    /*creat tree */
+	wilddog_node_addChild(p_head,p_augend);
+	wilddog_node_addChild(p_head,p_addend);
+	wilddog_node_addChild(p_head,p_sum);
+
+	/*set data tree */
+	wilddog_setValue(wilddog,p_head,set_callback,&set_finish);
+	/*abserver node */
+
+	/* release tree memory */	
+    wilddog_node_delete(p_head);
+	while(1){
+		if(set_finish)
+		{			
+			wilddog_addObserver(wilddog,WD_ET_VALUECHANGE,addObserver_callback,(void*)wilddog);
+			set_finish = FALSE;
+		}
+        wilddog_trySync();
+    }
+    wilddog_destroy(&wilddog);
+}
